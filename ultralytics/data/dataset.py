e4380ac956b4cc2020e39b08c5f67a15f7752827
tERaNCarol <wushangqiuxuan@126.com>
2024-06-23 17:19:08 +0800

    relative requirements

--- ultralytics/data/dataset.py
+++ ultralytics/data/dataset.py
@@ -178,7 +178,7 @@ class YOLODataset(BaseDataset):
             hyp.mosaic = hyp.mosaic if self.augment and not self.rect else 0.0
             hyp.mixup = hyp.mixup if self.augment and not self.rect else 0.0
             transforms = v8_transforms(self, self.imgsz, hyp)
-            # enhanced_transfuorms(self, self.imgsz, hyp)
+            # enhanced_transforms(self, self.imgsz, hyp)
         else:
             transforms = Compose([LetterBox(new_shape=(self.imgsz, self.imgsz), scaleup=False)])
         transforms.append(
